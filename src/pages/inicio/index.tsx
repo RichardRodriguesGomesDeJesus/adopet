86cefc0d3f35bd3b69e6ac773792b0a287231eaa
Richard <richardrodriguesgomesdejesus@gmail.com>
2022-10-25 20:28:29 -0300

    instalando o react-router-dom

---
+++ src/pages/inicio/index.tsx
@@ -0,0 +1,9 @@
+ export default function inicio () {
+    return(
+        <section>
+            <img src="" alt="" />
+            <h2>Boas-vindas!</h2>
+            <p>Que tal mudar sua vida adotando seu novo melhor amigo? Vem com a gente!</p>
+        </section>
+    )
+ }
